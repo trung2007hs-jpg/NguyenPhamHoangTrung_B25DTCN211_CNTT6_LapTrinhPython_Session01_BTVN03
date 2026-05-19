21bb891fcf2b9d730dccffe28b78157f83f8151d
trung2007hs-jpg <trung2007hs@gmail.com>
2026-05-19 10:29:36 +0700

    Update b3.py

--- b3.py
+++ b3.py
@@ -1,12 +1,12 @@
-# 1. Phân tích I/O
-# - Input: + Họ tên bệnh nhân (Kiểu chuỗi - `str`)
-#   + Mã bệnh án (Kiểu chuỗi - `str`)
-#   + Khoa/Phòng khám chỉ định (Kiểu chuỗi - `str`)
-# - Output: Phiếu khám bệnh điện tử hiển thị trên màn hình console thông qua các lệnh `print()`.
+# 1. Phân tích
+# - I: + Họ tên bệnh nhân str
+#   + Mã bệnh án str
+#   + Khoa/Phòng khám chỉ định str
+# - O: Phiếu khám bệnh điện tử hiển thị trên màn hình.
 
 # 2. Giải pháp
-# - Sử dụng lệnh `input()` để tiếp nhận thông tin từ bàn phím và gán vào các biến đơn lẻ.
-# - Sử dụng lệnh `print()` để thiết kế giao diện phiếu khám, 
+# - Sử dụng `input()` để tiếp nhập thông tin từ bàn phím.
+# - Sử dụng `print()` để thiết kế giao diện phiếu khám, 
 # truyền các biến đã nhập vào đúng vị trí hiển thị để tránh lỗi logic lệch thông tin.
 
 ten_benh_nhan = input("1. Nhập họ và tên bệnh nhân: ")
